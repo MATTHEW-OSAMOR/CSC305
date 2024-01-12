c5cd4a833be7689c0bb0f9821cf283276b96bd4e
MATTHEW-OSAMOR <93009832+MATTHEW-OSAMOR@users.noreply.github.com>
2024-01-12 16:45:57 +0100

    stuff

---
+++ hello_world_lib/target/debug/deps/hello_world_lib-b7dd1ab1014a1a2b.d
@@ -0,0 +1,5 @@
+C:\Users\maxos\OneDrive\Desktop\CSC305\hello_world_lib\target\debug\deps\libhello_world_lib-b7dd1ab1014a1a2b.rmeta: src\lib.rs
+
+C:\Users\maxos\OneDrive\Desktop\CSC305\hello_world_lib\target\debug\deps\hello_world_lib-b7dd1ab1014a1a2b.d: src\lib.rs
+
+src\lib.rs:
---
+++ hello_world_lib/target/debug/deps/hello_world_lib-fdd81437ddce8d00.d
@@ -0,0 +1,5 @@
+C:\Users\maxos\OneDrive\Desktop\CSC305\hello_world_lib\target\debug\deps\libhello_world_lib-fdd81437ddce8d00.rmeta: src\lib.rs
+
+C:\Users\maxos\OneDrive\Desktop\CSC305\hello_world_lib\target\debug\deps\hello_world_lib-fdd81437ddce8d00.d: src\lib.rs
+
+src\lib.rs:
